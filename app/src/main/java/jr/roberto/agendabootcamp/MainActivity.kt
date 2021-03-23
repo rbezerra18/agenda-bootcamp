d8c369ae5487e02e20ebe9dd807ac18147f4b4f2
Roberto Júnior <rbezerra18@hotmail.com>
2021-03-22 21:05:29 -0300

    Build app Criar evento no Google Agenda.

---
+++ app/src/main/java/jr/roberto/agendabootcamp/MainActivity.kt
@@ -0,0 +1,28 @@
+package jr.roberto.agendabootcamp
+
+import android.content.Intent
+import android.os.Bundle
+import android.provider.CalendarContract
+import android.provider.CalendarContract.Events.*
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val btnSetEvent: Button = findViewById(R.id.set_event)
+        btnSetEvent.setOnClickListener {
+            val intent = Intent(Intent.ACTION_INSERT)
+                .setData(CONTENT_URI)
+                .putExtra(TITLE, "Bootcamp everis")
+                .putExtra(EVENT_LOCATION, "on line")
+                .putExtra(CalendarContract.EXTRA_EVENT_BEGIN_TIME, System.currentTimeMillis())
+                .putExtra(CalendarContract.EXTRA_EVENT_END_TIME, System.currentTimeMillis() + (60*60*1000))
+
+            startActivity(intent)
+        }
+    }
+}
